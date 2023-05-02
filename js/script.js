74ea73a83fbe136ee12320501f04417919d8a9e7
Yasmin Silva <yasminsilvadeveloper@gmail.com>
2023-05-02 15:08:14 -0300

    Fazendo a IA jogar

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let buttons = document.querySelectorAll("#button-container button");
+let buttons = document.querySelectorAll(".button-container button");
 let messageContainer = document.querySelector("#message");
 let messageText = document.querySelector("#message p");
 let secondPlayer;
@@ -27,6 +27,12 @@ for(i = 0; i < boxes.length; i++) {
             // computar jogada
             if(player1 == player2) {
                 player1++;
+
+                if(secondPlayer == "ia-player") {
+                    // função para executar jogada
+                    computerPlay();
+                    player2++;
+                }
             } else {
                 player2++;
             }
@@ -35,6 +41,23 @@ for(i = 0; i < boxes.length; i++) {
             checkWinCondition();
         }
     })
+}
+
+// evento para saber se é 2 players ou IA
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+        secondPlayer = this.getAttribute("id");
+
+        // sumindo com os botões após o click e mostrando o jogo
+        for(let j = 0; j < buttons.length; j++) {
+            buttons[j].style.display = "none";
+        }
+
+        setTimeout(() => {
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+        }, 100);
+    });
 }
 
 // vê quem irá jogar
@@ -237,5 +260,31 @@ function declareWinner(winner) {
     let boxesToRemove = document.querySelectorAll(".box div");
     for(let i = 0; i < boxesToRemove.length; i++) {
         boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
+    }
+};
+
+// executar a lógica da jogada da CPU
+function computerPlay() {
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i = 0; i < boxes.length; i++) {
+        let randomNumber = Math.floor(Math.random() * 5);
+        // só preencher se o elemento filho estiver vazio
+        if(boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;
+                break;
+            }
+        // checagem de quantas estão preenchidas
+        } else {
+            filled++;
+        }
+    }
+
+    if(counter == 0 && filled < 9) {
+        computerPlay();
     }
 }
